74df9a53ec4174d13b8f686585634692970162da
HALtitor <e175753@ie.u-ryukyu.ac.jp>
2018-01-29 21:27:07 +0900

    first commit

--- report7.c
+++ report7.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
 
-void arrange(int array[], int size){
+void arrange(int scores[], int size){
+  //配列変数resultを用意
   const int Nsize = size;
-  int Nscores[Nsize];
+  int result[Nsize];
+  printf("scores = ");//引数のscores配列の出力
   for(int i=0; i<size; i++){
-    Nscores[i] = array[i];
+    printf("%d ",scores[i]);
+    result[i] = scores[i];
     for(int a = i; a>0; a--){
-      if(Nscores[a] < Nscores[a-1]){
-	int Otrade = Nscores[a];
-	int Ttrade = Nscores[a-1];
-	Nscores[a-1] = Otrade;
-	Nscores[a] = Ttrade;
+      //若い数字の配列と比較し並び替える
+      if(result[a] > result[a-1]){
+	int Otrade = result[a];
+	int Ttrade = result[a-1];
+	result[a-1] = Otrade;
+	result[a] = Ttrade;
       }
-      printf("data[%d] = %d\n",i,Nscores[i]);
     }
-    printf("data[%d] = %d\n",i,Nscores[i]);
+  }
+  printf("\nresult = ");//scoresの中身を大きい順に並べたresultを出力
+  for(int i=0; i<size; i++){
+    printf("%d ",result[i]);
   }
 }
 
 
 int main(){
-  const int SIZE = 5; //定数として配列のサイズを宣言                          
-  int scores[SIZE] = {0, 60, 30, 100, 90};
+  const int SIZE = 6; //定数として配列のサイズを宣言                          
+  int scores[SIZE] = {100, 60, 70, 100, 90, 80};
   //処理コード
   arrange(scores, SIZE);
   return 0;
